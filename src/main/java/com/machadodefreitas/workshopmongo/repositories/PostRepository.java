20d734019c5b8b680e15050c2d4627a29fe20ab4
Roneison Machado <roneison.machado@gmail.com>
2020-05-31 09:22:55 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/machadodefreitas/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.machadodefreitas.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.machadodefreitas.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
