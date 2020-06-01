369320033b587b8ed8b1b0acb8198b8c538f9df7
Roneison Machado <roneison.machado@gmail.com>
2020-06-01 08:34:12 -0300

    Consulta com varios criterios

--- src/main/java/com/machadodefreitas/workshopmongo/resources/util/URL.java
+++ src/main/java/com/machadodefreitas/workshopmongo/resources/util/URL.java
@@ -2,6 +2,9 @@ package com.machadodefreitas.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class URL {
 	
@@ -10,6 +13,15 @@ public class URL {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			return "";
+		}
+	}
+	
+	public static Date convertDate(String str, Date defaultDate) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			return format.parse(str);
+		} catch (ParseException e) {
+			return defaultDate;
 		}
 	}
 }
